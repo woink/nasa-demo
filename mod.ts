046c3b6c74a9d4724f35106e8c3d09d952c955b1
woink <wardprice@gmail.com>
2020-12-26 19:35:22 -0500

    setup default logger

--- mod.ts
+++ mod.ts
@@ -1,14 +1,27 @@
+import * as log from "https://deno.land/std/log/mod.ts"
 import { Application, send } from 'https://deno.land/x/oak@v6.4.1/mod.ts';
 import api from "./api.ts"
 
-
 const app = new Application();
+
 const PORT = 8000;
+
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("INFO"),
+  },
+  loggers: {
+    default: {
+      level: "INFO",
+      handlers: ["console"]
+    }
+  }
+})
 
 app.use(async (ctx, next) => {
 	await next();
 	const time = ctx.response.headers.get('X-Response-Time');
-	console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+	log.info(`${ctx.request.method} ${ctx.request.url}: ${time}`);
 });
 
 app.use(async (ctx, next) => {
@@ -37,6 +50,7 @@ app.use(async (ctx) => {
 });
 
 if (import.meta.main) {
+  log.info(`Starting server on port ${PORT}....`)
 	await app.listen({
 		port: PORT,
 	});
--- models/planets.ts
+++ models/planets.ts
@@ -53,7 +53,7 @@ async function loadPlanetsData() {
 }
 
 planets = await loadPlanetsData();
-console.log(`${planets.length} habitable planets found!`)
+log.info(`${planets.length} habitable planets found!`)
 
 export function getAllPlanets() {
 	return planets
